3a49a7f859d0a414c8689bfb0a267164d794fe4d
MakKam26 <kamzra@googlemail.com>
2024-01-05 22:07:08 +0000

    add another test file

---
+++ test_file_one.java
@@ -0,0 +1,13 @@
+public class test_file_one{
+    public static void main(String[] args){
+        System.out.println("Just a final test to understand");
+        System.out.println(2*2);
+
+
+    }
+
+
+
+
+
+}
